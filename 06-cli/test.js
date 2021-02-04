53b743769dc75371ac55c747ca467e25fe8dc93a
Rafael Pérez <rapzadev@gmail.com>
2021-02-04 15:24:42 -0300

    Manipulando arquivos com nodejs - CREATE

--- 06-cli/test.js
+++ 06-cli/test.js
@@ -2,6 +2,7 @@ const { deepStrictEqual, ok  } = require('assert')
 
 const database = require('./database')
 
+
 const DEFAULT_ITEM_CADASTRAR = {
     nome: 'Flash',
     poder: 'Speed',
@@ -10,6 +11,10 @@ const DEFAULT_ITEM_CADASTRAR = {
 
 describe('Suite de manipulação de Herois', () => {
     
+    before(async () => {
+        await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+    })
+
     it('Deve pesquisar um heroi usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR
         const [resultado] = await database.listar(expected.id)
@@ -17,9 +22,17 @@ describe('Suite de manipulação de Herois', () => {
         deepStrictEqual(resultado, expected)
     })
 
-    // it('Deve cadastrar um heroi, usando arquivos', async () => {
-    //     const expected = DEFAULT_ITEM_CADASTRAR
+    it('Deve cadastrar um heroi, usando arquivos', async () => {
+        const expected = {
+            nome: 'Batman',
+            poder: 'Astucia',
+            id: 2,
+        }
 
-    //     ok(null, expected)
-    // })
+        await database.cadastrar(expected)
+
+        const [actual] = await database.listar(expected.id)
+
+        deepStrictEqual(actual, expected)
+    })
 })
